cc9c6ba25d8e75797e42308e986258f79da14bc2
Runnard <79583885+Runnard@users.noreply.github.com>
2021-05-21 15:07:05 +0200

    Add files via upload

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("hello");
+// les lib : 
+//-Chart.js https://www.chartjs.org/docs/latest/
+//-apexchart https://apexcharts.com/ 
+
+ScrollTrigger.create({
+    trigger: "#body",
+    start: "top top",
+    end: "bottom bottom",
+    pin:"#body"
+  });
+
